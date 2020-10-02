c77dede390cc7e63a381c8de787b0d353b3c7da8
StrasserNiklas <niklasstrasser@hotmail.com>
2020-10-02 11:34:28 +0200

    first

---
+++ Mandelbrot/Complex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mandelbrot
+{
+    public class Complex
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        
+        public Complex(double a, double b)
+        {
+            this.A = a;
+            this.B = b;
+        }
+
+        public void Square()
+        {
+            double temp = (this.A * this.A) - (this.B * this.B);
+            this.B = 2.0 * this.A * this.B;
+            this.A = temp;
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt((this.A * this.A) + (this.B * this.B));
+        }
+
+        public void Add(Complex c)
+        {
+            this.A += c.A;
+            this.B += c.B;
+        }
+    }
+
+}
---
+++ Mandelbrot/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mandelbrot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            var bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            for (int x = 0; x < pictureBox1.Width; x++)
+            {
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    double a = (double)(x - (pictureBox1.Width / 2)) / (double)(pictureBox1.Width / 4);
+                    double b = (double)(y - (pictureBox1.Height / 2)) / (double)(pictureBox1.Height / 4);
+
+                    var c = new Complex(a, b);
+                    var z = new Complex(0, 0);
+
+                    var iteration = 0;
+
+                    do
+                    {
+                        iteration++;
+                        z.Square();
+                        z.Add(c);
+
+                        if (z.Magnitude() > 2.0)
+                        {
+                            break;
+                        }
+                    } 
+                    while (iteration < 100);
+
+                    bitmap.SetPixel(x, y, iteration < 100 ? Color.Black : Color.White);
+
+                }
+            }
+
+            pictureBox1.Image = bitmap;
+        }
+    }
+}
